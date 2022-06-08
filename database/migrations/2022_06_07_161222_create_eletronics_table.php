d6f805d28acbd0c958e3b5bd806703517afd435a
Davidhsaiou <t437047t@hotmail.com>
2022-06-08 15:59:29 +0800

    add models, and migrations tmp

---
+++ database/migrations/2022_06_07_161222_create_eletronics_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\StorageArea;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEletronicsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('eletronics', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 128);
+//            $table->
+            $table->integer('count');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('eletronics');
+    }
+}
